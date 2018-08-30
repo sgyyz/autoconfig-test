90c152765efa20d58e78a4a3a64d8e58949ba952
Yongzhi Yang <yongzhi.yang@57blocks.io>
2018-08-30 12:04:55 +0800

    Initial Commit

---
+++ src/main/java/com/troy/springboot/autoconfigtest/AutoconfigTestApplication.java
@@ -0,0 +1,12 @@
+package com.troy.springboot.autoconfigtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoconfigTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoconfigTestApplication.class, args);
+	}
+}
